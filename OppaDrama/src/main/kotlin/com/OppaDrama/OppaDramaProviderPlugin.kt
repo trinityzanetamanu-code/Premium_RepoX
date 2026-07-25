385dd4c3d9a8c702c789fbf28119861a07a2a6d5
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-07-25 10:35:46 +0900

    Update OppaDramaProviderPlugin.kt

--- OppaDrama/src/main/kotlin/com/OppaDrama/OppaDramaProviderPlugin.kt
+++ OppaDrama/src/main/kotlin/com/OppaDrama/OppaDramaProviderPlugin.kt
@@ -1,7 +1,7 @@
 package com.OppaDrama
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugi
 
 @CloudstreamPlugin
 class OppaDramaPlugin : BasePlugin() {
