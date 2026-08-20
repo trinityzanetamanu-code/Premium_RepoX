700e122edde4b4b41b91505d572d3aead292d81d
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-08-20 13:04:36 +0900

    Update MovieboxPlugin.kt

--- Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
+++ Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
@@ -7,6 +7,9 @@ import android.content.Context
 @CloudstreamPlugin
 class MovieboxPlugin : Plugin() {
     override fun load(context: Context) {
+        // Identity persisten per-instalasi disiapkan sebelum provider dipakai.
+        MovieBoxProvider.attachContext(context)
+
         // Registrasi provider utama
         registerMainAPI(MovieBoxProvider())
     }
