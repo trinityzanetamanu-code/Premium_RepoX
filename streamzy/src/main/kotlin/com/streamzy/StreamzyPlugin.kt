25ca3bf062a29c19254b98924eda5826eb28ef5b
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-08-25 03:54:46 +0900

    Create StreamzyPlugin.kt

---
+++ streamzy/src/main/kotlin/com/streamzy/StreamzyPlugin.kt
@@ -0,0 +1,15 @@
+package com.streamzy
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class StreamzyPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(
+            StreamzyProvider()
+        )
+    }
+}
