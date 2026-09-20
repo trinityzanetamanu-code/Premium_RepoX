9fefaade8de4d425ac598407a99f0d19eec75f6e
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-09-20 20:59:31 +0900

    Use local VidSrc engine in AdiFilmSemi

--- AdiFilmSemi/src/main/kotlin/com/michat88/AdiFilmSemiPlugin.kt
+++ AdiFilmSemi/src/main/kotlin/com/michat88/AdiFilmSemiPlugin.kt
@@ -1,7 +1,6 @@
 package com.michat88
 
 import android.content.Context
-import com.Adicinemax21.Adicinemax21VidSrcShared
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
@@ -11,8 +10,8 @@ class AdiFilmSemiPlugin : Plugin() {
         // Exact MovieBox runtime profile harus siap sebelum request playback pertama.
         AdiFilmSemiExtractor.attachContext(context)
 
-        // Reuse exact VidSrc/WebView/WASM engine milik Adicinemax21.
-        Adicinemax21VidSrcShared.attachContext(context)
+        // Attach local VidSrc/WebView/WASM engine.
+        AdiFilmSemiVidSrcShared.attachContext(context)
 
         // Provider utama: MovieBox/VidSrc proven + Idlix prefetch/cache.
         registerMainAPI(AdiFilmSemiPrefetchProvider())
