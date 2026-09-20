b542eca05d51f8b96398030b11cc1fc8ef69e9ff
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-09-20 21:00:30 +0900

    Make Adicinemax21 Idlix single-path

--- Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21.kt
+++ Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21.kt
@@ -2,7 +2,6 @@ package com.Adicinemax21
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.Adicinemax21.Adicinemax21Extractor.invokeMoviebox
-import com.Adicinemax21.Adicinemax21Idlix.invokeIdlix
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.metaproviders.TmdbProvider
@@ -322,18 +321,6 @@ open class Adicinemax21 : TmdbProvider() {
                     res.altTitle,
                     res.year,
                     res.airedYear,
-                    res.season,
-                    res.episode,
-                    subtitleCallback,
-                    callback
-                )
-            },
-            {
-                invokeIdlix(
-                    res.title ?: return@runAllAsync,
-                    res.orgTitle,
-                    res.altTitle,
-                    res.year,
                     res.season,
                     res.episode,
                     subtitleCallback,
